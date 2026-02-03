67c8de1c5e1da6374e8a6daf455eef9b514354c5
opsora-agency <opsoraagency@gmail.com>
2026-02-03 15:35:51 +0530

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,13 +15,11 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  Scalable, Reliable, and Future-Ready Digital Solutions Engineered for Sustainable Growth
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                  Opsora Agency is your comprehensive digital growth partner delivering end-to-end marketing automation, 
+                  CRM integration, and performance-driven strategies designed to build sustainable revenue pipelines and operational efficiency for modern businesses.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
@@ -29,8 +27,18 @@ const Hero = () => {
                     href="https://github.com/themewagon/startup-nextjs"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    Star on GitHub
+                    Contact Now
                   </Link>
+
+                  <Link
+                    target="_blank"
+                    href="https://github.com/themewagon/startup-nextjs"
+                    className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5">
+                    Book Live Demo
+                  </Link>
+
+
+                  
                 </div>
               </div>
             </div>
