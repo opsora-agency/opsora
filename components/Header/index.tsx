401863dd2bc104a8156518d412d0814a4a644e0a
opsora-agency <opsoraagency@gmail.com>
2026-02-03 15:48:29 +0530

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -161,16 +161,16 @@ const Header = () => {
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
-                  href="/signin"
+                  href="https://opsora-pulse-panel.lovable.app/"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
                 >
-                  Sign In
+                  Client Login
                 </Link>
                 <Link
-                  href="/signup"
+                  href="https://wa.me/918401765505"
                   className="ease-in-up hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white shadow-btn transition duration-300 hover:bg-opacity-90 hover:shadow-btn-hover md:block md:px-9 lg:px-6 xl:px-9"
                 >
-                  Sign Up
+                  Contact Now
                 </Link>
                 <div>
                   <ThemeToggler />
